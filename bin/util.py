e7744d49337dc47d04d4d9066705ec7e5aa6e48e
David Von Lehman <david.vonlehman@gmail.com>
2013-10-07 15:29:56 -0700

    fixing bug

--- bin/util.py
+++ bin/util.py
@@ -2,5 +2,7 @@ import inflection
 
 def underscore_keys(obj):
 	for key in obj.keys():
-		obj[inflection.underscore(key)] = obj[key]
-		del obj[key]
+		new_key = inflection.underscore(key)
+		if new_key != key:
+			obj[inflection.underscore(key)] = obj[key]
+			del obj[key]
