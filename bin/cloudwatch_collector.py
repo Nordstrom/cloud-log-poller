f7ae0fb23c0568e0eb6ee726d4bf08c61631e694
David Von Lehman <david.vonlehman@gmail.com>
2013-10-08 16:32:52 -0700

    support for DynamoDB metrics

--- bin/cloudwatch_collector.py
+++ bin/cloudwatch_collector.py
@@ -2,6 +2,7 @@ import os
 import boto.ec2.cloudwatch
 import datetime
 import logging
+import inflection
 from datetime import datetime as dt
 
 STATISTICS = ['Minimum', 'Maximum', 'Sum', 'Average', 'SampleCount']
@@ -19,6 +20,23 @@ class CloudWatchCollector():
       aws_secret_access_key=config['aws_secret_access_key'])
 
 
+  def process_metrics(self, metric_name, namespace, dimensions, start_time, end_time):
+    datapoints = self.cloudwatch.get_metric_statistics(60, start_time, end_time, metric_name, namespace, STATISTICS, dimensions=dimensions)
+
+    metric_display = [namespace, metric_name]
+    metric_display.extend(dimensions.values())
+    logger.info("Received %s datapoints for Cloudwatch metric %s" % (len(datapoints), ':'.join(metric_display)))
+
+    if len(datapoints) == 0:
+      return
+
+    # Append the metric_name onto each datapoint
+    for datapoint in datapoints:
+      datapoint['metric'] = metric_name
+
+    for transport in self.__transports:   
+      transport.send(namespace, 'cloudwatch', datapoints)
+
   def run(self):
     end_time = dt.utcnow()
     if self.__last_run:
@@ -32,22 +50,20 @@ class CloudWatchCollector():
     logger.info("Running CloudwatchCollector on %s metrics between %s and %s" % 
       (len(self.__config['metrics']), start_time, end_time))
 
-    events = []
-
     for metric in self.__config['metrics']:
       # TODO: Support for dimensions for dynamo table names
       # http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/MonitoringDynamoDB.html
-      namespace, metric_name = metric.split(':')
-      datapoints = self.cloudwatch.get_metric_statistics(60, start_time, end_time, metric_name, namespace, STATISTICS)
-      if len(datapoints) > 0:
-        logger.info("Received %s datapoints for Cloudwatch metric %s:%s" % (len(datapoints), namespace, metric_name))
-      for datapoint in datapoints:
-        datapoint['metric'] = metric_name
-        logger.debug("Cloudwatch datapoint: " + repr(datapoint))
-        events.append(datapoint)
+      namespace = metric['namespace']
+      metric_name = metric['metric_name']
 
-      logger.info("Received %s datapoints for Cloudwatch metric %s:%s" % (len(datapoints), namespace, metric_name))
-      if (len(events) > 0):
-        for transport in self.__transports:   
-          transport.send(namespace, 'cloudwatch', events)
-
+      # Collect the remaining metric dimensions besides metric_name and namespace
+      dimensions = {}
+      for key in metric:
+        if not key in ('metric_name', 'namespace'):
+          dimensions[inflection.camelize(key)] = metric[key]
+      
+      # If metric_name is a list, process each seperately with the same values for namespace and dimensions
+      if isinstance(metric_name, list):
+        [self.process_metrics(name, namespace, dimensions, start_time, end_time) for name in metric_name]
+      else:
+        self.process_metrics(metric_name, namespace, dimensions, start_time, end_time)
